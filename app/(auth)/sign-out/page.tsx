a6747ad25269c5891f569762e5a51ae096545dfd
maggioniduffy <fausmaggioni5@gmail.com>
2025-03-07 18:25:37 -0300

    .

---
+++ app/(auth)/sign-out/page.tsx
@@ -0,0 +1,5 @@
+import AuthForm from "@/components/AuthForm";
+
+const page = () => <AuthForm type="sign-up" />;
+
+export default page;
---
+++ components/AuthForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface Props {
+  type: "sign-in" | "sign-up";
+}
+const AuthForm = ({ type }: Props) => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;
