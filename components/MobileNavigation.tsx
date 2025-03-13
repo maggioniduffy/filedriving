7a379ed70c4087982a270afa25d4761584cd22c5
maggioniduffy <fausmaggioni5@gmail.com>
2025-03-13 13:02:36 -0300

    init mobile nav

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+"use client";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const MobileNavigation = () => {
-  return <div>MobileNavigation</div>;
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+  return (
+    <header className="mobile-header">
+      <Image
+        src="/assets/icons/logo-full-brand.svg"
+        alt="Logo"
+        width={120}
+        height={52}
+        className="h-auto"
+      />
+
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image
+            src={"/assets/icons/menu.svg"}
+            alt="search"
+            width={30}
+            height={30}
+          />
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Are you absolutely sure?</SheetTitle>
+            <SheetDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </SheetDescription>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
 };
 
 export default MobileNavigation;
