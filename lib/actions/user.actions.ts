48225ce312e1bf8e1e69f16b7502ccdedb7f0921
maggioniduffy <fausmaggioni5@gmail.com>
2025-03-13 11:34:35 -0300

    otp redirecting all right

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,10 +4,16 @@ import { ID, Query } from "appwrite";
 import { createAdminClient } from "../appwrite";
 import { appWriteConfig } from "../appwrite/config";
 import { parseStringify } from "../utils";
+import { cookies } from "next/headers";
 
-interface createAccountDto {
+interface createAccountDTO {
   fullName: string;
   email: string;
+}
+
+interface VerifySecretDTO {
+  accountId: string;
+  password: string;
 }
 
 const getUserByEmail = async (email: string) => {
@@ -26,7 +32,7 @@ const handleError = (error: unknown, message: string) => {
   throw error;
 };
 
-const sendEmailOTP = async (email: string) => {
+export const sendEmailOTP = async (email: string) => {
   const { account } = await createAdminClient();
   try {
     const session = await account.createEmailToken(ID.unique(), email);
@@ -36,7 +42,7 @@ const sendEmailOTP = async (email: string) => {
   }
 };
 
-export const createAccont = async ({ fullName, email }: createAccountDto) => {
+export const createAccont = async ({ fullName, email }: createAccountDTO) => {
   const existingUser = await getUserByEmail(email);
   const accountId = await sendEmailOTP(email);
 
@@ -59,4 +65,24 @@ export const createAccont = async ({ fullName, email }: createAccountDto) => {
   }
 
   return parseStringify({ accountId });
+};
+
+export const verifySecret = async ({
+  accountId,
+  password,
+}: VerifySecretDTO) => {
+  try {
+    const { account } = await createAdminClient();
+    const session = await account.createSession(accountId, password);
+    (await cookies()).set("appwrite-session", session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true,
+    });
+
+    return parseStringify({ sessionId: session.$id });
+  } catch (error) {
+    handleError(error, "Failed to verify secret");
+  }
 };
